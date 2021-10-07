d1c25825d75ea43bc6ae1c0f930e2083cecd4a60
Abdullahi Aden <Abbe6152@gmail.com>
2021-10-07 15:19:48 +0200

    form

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,37 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">this is client</div>;
+  return (
+    <div className="container">
+      <h1>Todo App</h1>
+      <div className="row">
+        <form>
+          <div className="form-group col">
+            <label htmlFor="todoInput">Todo</label>
+            <input
+              type="text"
+              className="form-control"
+              id="todoInput"
+              placeholder="Enter Todo"
+            />
+          </div>
+          <div className="form-group col">
+            <label htmlFor="contentInput">Content</label>
+            <input
+              type="text"
+              className="form-control"
+              id="contentInput"
+              placeholder="Enter content"
+            />
+          </div>
+
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default App;
