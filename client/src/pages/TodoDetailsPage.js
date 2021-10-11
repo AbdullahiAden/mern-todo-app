e8dfe9a9882b032449361cdb8e01a2f41c7774fe
Abdullahi Aden <Abbe6152@gmail.com>
2021-10-11 17:56:32 +0200

    fixed todo details

--- client/src/pages/TodoDetailsPage.js
+++ client/src/pages/TodoDetailsPage.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { Redirect, useHistory } from "react-router";
+import { Link } from "react-router-dom";
 
 const TodoDetailsPage = (props) => {
   const { id } = props.match.params;
   const api = ` http://localhost:5000/api/todoApp/${id}`;
 
   const [singleTodo, setSingleTodo] = useState([]);
+  const [newTodo, setNewdTodo] = useState({ title: "", content: "" });
+  const [updatedTodo, setUpdatedTodo] = useState({ title: "", content: "" });
   const history = useHistory();
 
   //   get the details of a single todo
@@ -22,8 +25,21 @@ const TodoDetailsPage = (props) => {
 
   //   delete todo
   async function deleteTodo(e) {
-    await fetch(api, {
+    const url = ` http://localhost:5000/api/todoApp/${id}/delete `;
+
+    await fetch(url, {
       method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(() => history.push("/todoApp"));
+  }
+  //   update todo --------
+  async function updateTodo(e) {
+    await fetch(api, {
+      method: "PUT",
+      body: JSON.stringify(updatedTodo),
+
       headers: {
         "Content-Type": "application/json",
       },
@@ -31,30 +47,45 @@ const TodoDetailsPage = (props) => {
   }
 
   return (
-    <div className="conatiner shadow m-3">
+    <div className="conatiner shadow m-3 p-3 rounded border-bottom border-primary text-light">
       {singleTodo.length < 0 ? (
         <h1>Loading</h1>
       ) : (
         <div>
-          {singleTodo.map((todo, index) => {
-            return (
-              <div key={index} className=" shadow ">
-                <h1>
-                  Title:{" "}
-                  <span className="border-3 border-primary border-bottom">
-                    {todo.title}
-                  </span>{" "}
-                </h1>
-                <h1>Content: {todo.content}</h1>
-                {/*  ONLY DATE ----------------------------- */}
-                <p>Last Edit: {todo.updatedAt}</p>
+          <div>
+            <h1>
+              Title:{" "}
+              <span className="border-3 border-primary ">
+                {singleTodo.title}
+              </span>{" "}
+            </h1>
 
-                <button onClick={deleteTodo} className="btn btn-danger">
-                  Delete
-                </button>
-              </div>
-            );
-          })}
+            <h1>Content: {singleTodo.content}</h1>
+            {/* get only the first 10 char, which the year, month, day */}
+            <div className="py-3">
+              {" "}
+              {singleTodo.updatedAt && (
+                <p>Updated Last: {singleTodo.updatedAt.slice(0, 10)}</p>
+              )}
+            </div>
+
+            {/* <Link to={`/todoApp/${todo._id}/update`}>Update</Link> */}
+
+            <div>
+              <button
+                type="button"
+                className="btn btn-success m-2"
+                data-toggle="modal"
+                data-target="#exampleModal"
+              >
+                Update
+              </button>
+
+              <button onClick={deleteTodo} className="btn btn-danger ">
+                Delete
+              </button>
+            </div>
+          </div>
         </div>
       )}
     </div>
