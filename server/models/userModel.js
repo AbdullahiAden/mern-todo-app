3b9806fe7c0796413abbfc48f599dbb465bbc2c9
Abdullahi Aden <Abbe6152@gmail.com>
2021-10-15 17:38:04 +0200

    moved password hash function to model

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose")
+const bcrypt= require("bcryptjs")
+
 const userSchema= new mongoose.Schema({
     email:{
         type:String,
@@ -10,6 +12,13 @@ const userSchema= new mongoose.Schema({
     }
    
    
+})
+// fire a function to hash signed up user's password before it's saved to db
+userSchema.pre("save", async function (next){
+    const salt =await bcrypt.genSalt()
+    this.password= await bcrypt.hash(this.password, salt)
+    next()
+
 })
 
 
