805315196de52df482cd211822dab1b20af0b0dd
Abdullahi Aden <Abbe6152@gmail.com>
2021-10-16 10:22:48 +0200

    login page

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,42 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const LoginPage = () => {
+  const[ signUser, setSignUser ] = useState([]);
+  //   sign in user
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    const api = "http://localhost:5000/login";
+
+    await fetch(api, {
+      method: "POST",
+      body: JSON.stringify(signUser),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setSignUser(data);
+        console.log(data);
+        // history.push("/")
+      });
+  };
   return (
     <div className="container mt-5">
       <h1>Login </h1>
 
       <div className="container">
-        <form>
+        <form onSubmit={handleOnSubmit}>
           <div className="form-group my-3">
-            <label htmlFor="exampleInputEmail1">Email address</label>
+            <label htmlFor="email">Email address</label>
             <input
               type="email"
+              id="email"
               className="form-control"
-              id="exampleInputEmail1"
               aria-describedby="emailHelp"
               placeholder="Enter email"
+              onChange={(e) => setSignUser({ ...signUser, email: e.target.value })}
             />
           </div>
           <div className="form-group">
-            <label htmlFor="exampleInputPassword1">Password</label>
+            <label htmlFor="password">Password</label>
             <input
               type="password"
               className="form-control"
-              id="exampleInputPassword1"
+              id="password"
               placeholder="Password"
+              onChange={(e) => setSignUser({ ...signUser, password: e.target.value })}
             />
           </div>
           <div className="form-check"></div>
-          <button type="submit" className="btn btn-primary">
-            Login
-          </button>
+          <button className="btn btn-primary">Login</button>
         </form>
 
-        <Link to="/signUp"> <button type="submit" className="btn btn-outline-primary mt-3">
-          Register
-        </button></Link>
-       
+        <Link to="/signUp">
+          {" "}
+          <button type="submit" className="btn btn-outline-primary mt-3">
+            Register
+          </button>
+        </Link>
       </div>
     </div>
   );
