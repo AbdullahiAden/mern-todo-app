018b75e92c21d0921791fae775628ecdf0a2b180
Abdullahi Aden <Abbe6152@gmail.com>
2021-10-16 17:18:52 +0200

    static method to login user

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,25 +1,35 @@
-const mongoose = require("mongoose")
-const bcrypt= require("bcryptjs")
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const userSchema= new mongoose.Schema({
-    email:{
-        type:String,
-        required:true
-    },
-    password:{
-        type:String,
-        required:true
-    }
-   
-   
-})
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
 // fire a function to hash signed up user's password before it's saved to db
-userSchema.pre("save", async function (next){
-    const salt =await bcrypt.genSalt()
-    this.password= await bcrypt.hash(this.password, salt)
-    next()
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
-})
+// static method to login user
+userSchema.statics.loginUser = async function (email, password) {
+  const user = await this.findOne({ email:email });
+  if (user) {
+    // compare the password that is entered, with this.password (hashed) from db
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user;
+    }
+    throw Error("Incorrect Password");
+  }
+  throw Error("Incorrect Email");
+};
 
-
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model("User", userSchema);
