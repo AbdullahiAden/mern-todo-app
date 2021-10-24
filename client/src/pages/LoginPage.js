6236f098861186cbc54e553a43f37f63498c1389
Abdullahi Aden <Abbe6152@gmail.com>
2021-10-24 10:52:33 +0200

    sign in

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,26 +1,40 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 const LoginPage = () => {
-  const[ signUser, setSignUser ] = useState([]);
+  const [signUser, setSignUser] = useState([]);
+  const history = useHistory();
+
   //   sign in user
   const handleOnSubmit = async (e) => {
     e.preventDefault();
     const api = "http://localhost:5000/login";
 
-    await fetch(api, {
-      method: "POST",
-      body: JSON.stringify(signUser),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        setSignUser(data);
-        console.log(data);
-        // history.push("/")
-      });
+    try {
+      await fetch(api, {
+        method: "POST",
+        body: JSON.stringify(signUser),
+        
+        headers: {
+          "Content-Type": "application/json",
+          withCredentials: true, credentials: 'include'
+        },
+
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setSignUser(data);
+          console.log(data);
+          if (data.user) {
+            localStorage.setItem('jwt', data.jwt);
+
+            history.push("/todoApp");
+            console.log(signUser);
+          }
+        });
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
     <div className="container mt-5">
@@ -36,7 +50,9 @@ const LoginPage = () => {
               className="form-control"
               aria-describedby="emailHelp"
               placeholder="Enter email"
-              onChange={(e) => setSignUser({ ...signUser, email: e.target.value })}
+              onChange={(e) =>
+                setSignUser({ ...signUser, email: e.target.value })
+              }
             />
           </div>
           <div className="form-group">
@@ -46,7 +62,9 @@ const LoginPage = () => {
               className="form-control"
               id="password"
               placeholder="Password"
-              onChange={(e) => setSignUser({ ...signUser, password: e.target.value })}
+              onChange={(e) =>
+                setSignUser({ ...signUser, password: e.target.value })
+              }
             />
           </div>
           <div className="form-check"></div>
@@ -56,7 +74,7 @@ const LoginPage = () => {
         <Link to="/signUp">
           {" "}
           <button type="submit" className="btn btn-outline-primary mt-3">
-            Register
+            Sign up
           </button>
         </Link>
       </div>
